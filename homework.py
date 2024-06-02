e26f3d6e7d49cc0fe11a9ece7143477ccfc46973
Datunamikava <167118372+Datunamikava@users.noreply.github.com>
2024-06-02 16:49:28 +0300

    Add files via upload

--- homework.py
+++ homework.py
@@ -1,31 +1,39 @@
-name=“datuna”
-
-last_name=“mikava”
-
-age=14
-
-print (name + last_name +  str(age) )
+massage="game Over"
+print(massage)
 
 
-
-
-
-
-
-
-
-
-
+price=139
+print(139)
 
 
 
 print(10+10)
-print(5*5)
-print(10//2)
-print(18-7)
-print(100-21)
-print(18*18)
-print(22+23)
-print(11*11)
-print(18//2)
-print(100//2)
+print(22-11)
+print(44*12)
+print(17//2)
+print(23+44)
+print(22//2)
+print(31-23)
+print(12+4)
+print(18+18)
+print(100+1000)
+print(123+1234)
+print(1982//2)
+print(12*18)
+print(37+37)
+print(12//2)
+print(15/2)
+
+
+
+
+name="datuna"
+
+last_name="mikava"
+
+age=14
+
+print(name + last_name + str(age) )
+
+
+
