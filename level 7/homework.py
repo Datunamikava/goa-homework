c7924fd27d32b37a0ab8417d29df00586460a0f1
datuna <datunamikava000@gmail.com>
2024-07-06 12:43:12 +0300

    commit

---
+++ level 7/homework.py
@@ -0,0 +1,47 @@
+age1 = int(input("enter a number: "))
+print (age1)
+ 
+age2=int(input("enter an number: "))
+print (age2)
+
+age3=int(input("enter an number: "))
+print (age3)
+
+age4=int(input("enter an number: "))
+print (age4)
+
+print(age1+age2+age3-age4)
+
+age1=int(input("enter an number:"))
+print (age1)
+
+print(36+20)
+
+
+age2=int(input("enter an number:"))
+print (age2)
+print (33+20)
+
+
+
+age3=int(input("enter an number:"))
+print (age3)
+print(56+20)
+
+
+age4=int(input("enter an number:"))
+print (age4)
+print(57+20)
+
+
+name="datuna"
+
+last_name="mikava"
+
+age=14
+
+print(name + last_name + str(age) )
+
+
+
+
